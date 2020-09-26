bed39143b66107c272eb3159a63ae8d75bf14c8b
mik25 <25mik25@gmail.com>
2020-09-26 07:53:03 +0100

    trying

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  name: 'marveltv', // unique addon name
-  prefix: 'marveltv', // unique addon prefix (must be different for all addons!)
-  icon: 'https://github.com/mik25/gitaddon.git/logo.png', // set the correct domain name here so the logo is accessible
-  description: 'Mixed category iptv.', // addon description
+  name: 'Self Service IPTV', // unique addon name
+  prefix: 'Self Service_', // unique addon prefix (must be different for all addons!)
+  icon: 'https://helpyourself.surge.sh/logo.png', // set the correct domain name here so the logo is accessible
+  description: 'USA / UK / Canadian / Various Mixed Category IPTV / HD channels.', // addon description
   connLimit: false, // can be false or an integer, if set to an integer, it will show a message with "Limited to X connections."
   paginate: 100, // how many catalogs to show per page
   playlistUrl: 'http://marveldc.best/get.php?username=mikes3pm2019&password=MyTgDlKv5643871&type=m3u_plus', // the URL to the M3U playlist
